79054038e7d1c6a4205b59d043bfef2024517e5d
openwrtTee <165190199+openwrtTee@users.noreply.github.com>
2025-07-27 12:02:18 +0800

    Update update.sh
    Signed-off-by: openwrtTee <165190199+openwrtTee@users.noreply.github.com>

--- update.sh
+++ update.sh
@@ -985,10 +985,10 @@ main() {
     update_script_priority
     fix_easytier
     update_geoip
-    update_package "runc" "releases" "v1.2.6"
-    update_package "containerd" "releases" "v1.7.27"
-    update_package "docker" "tags" "v28.2.2"
-    update_package "dockerd" "releases" "v28.2.2"
+    update_package "runc" "releases" "v1.3.0"
+    update_package "containerd" "releases" "v1.7.28"
+    update_package "docker" "tags" "v28.3.2"
+    update_package "dockerd" "releases" "v28.3.2"
     # update_package "xray-core"
     # update_proxy_app_menu_location
     # update_dns_app_menu_location
