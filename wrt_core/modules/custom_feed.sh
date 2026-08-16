289bb92b34df867634a36879865224f7c84a6efa
openwrtTee <165190199+openwrtTee@users.noreply.github.com>
2026-08-16 19:26:10 +0800

    Update custom_feed.sh
    Signed-off-by: openwrtTee <165190199+openwrtTee@users.noreply.github.com>

--- wrt_core/modules/custom_feed.sh
+++ wrt_core/modules/custom_feed.sh
@@ -195,6 +195,7 @@ install_custom_feed() {
         "sbwml/luci-app-mosdns|https://github.com/sbwml/luci-app-mosdns.git|v5|mosdns luci-app-mosdns"
         "Openwrt-Passwall/openwrt-passwall|https://github.com/Openwrt-Passwall/openwrt-passwall.git|main|luci-app-passwall"
         "nikkinikki-org/OpenWrt-nikki|https://github.com/nikkinikki-org/OpenWrt-nikki.git|main|nikki luci-app-nikki mihomo-meta"
+        "VIKINGYFY/packages|https://github.com/VIKINGYFY/packages.git|main|sing-box luci-app-homeproxy"
     )
 
     feeds_path=$(get_feeds_path)
